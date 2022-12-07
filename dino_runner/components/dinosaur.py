53e1bd6436f8ceaa3179ecb56d42175cb25ba901
Ithan5 <Johan.Aguirre@fundacion-jala.org>
2022-12-07 19:46:59 -0400

    Class3:Obstacles

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -32,7 +32,7 @@ class Dinosaur(Sprite):
             self.dino_run = False
             self.dino_duck = True
             self.dino_jump = False
-        elif user_input[pygame.K_UP] and not self.dino_jump:
+        elif user_input[pygame.K_UP] and not self.dino_duck:
             self.dino_run = False
             self.dino_duck = False
             self.dino_jump = True
